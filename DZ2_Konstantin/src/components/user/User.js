adfbedf44a718822926f380784f9b51d3a440038
KonstantinPyCharm <153346201+KonstantinPyCharm@users.noreply.github.com>
2024-02-06 18:31:19 +0600

    Add files via upload

---
+++ DZ2_Konstantin/src/components/user/User.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const User = ({content}) => {
+    return (
+        <div>
+            <h1>
+                {content}
+            </h1>
+        </div>
+    );
+};
+
+export default User;
